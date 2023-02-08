6eaeb2e68d2af4d9aac7c8a64c7f809a01b9dcbf
strawhattom <xie.tom7@gmail.com>
2023-02-08 19:04:59 +0100

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -9,10 +9,17 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
-		csrf: {
-			checkOrigin: false
-		}
+		// default options are shown
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
 	}
 };
 
